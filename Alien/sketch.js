9bcad45ad9a99a3126376874db50865bfef525ab
LavanSambavan <lavansambavan@gmail.com>
2024-09-13 12:17:01 -0600

    alien work start

--- Alien/sketch.js
+++ Alien/sketch.js
@@ -8,8 +8,16 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
 }
 
 function draw() {
   background(220);
+  noStroke()
+
+  fill(100, 255, 150);
+  circle(width/2,height/2,100);
+  rect(width/2,height/2+50,100);
+  rect(width/2-40,height/2+100,30)
+
 }
