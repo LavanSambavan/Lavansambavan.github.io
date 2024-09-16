770d467821afbee1a156fde2a37af386684db32f
LavanSambavan <lavansambavan@gmail.com>
2024-09-16 12:16:27 -0600

    Start of scene

---
+++ Interactive Scene/sketch.js
@@ -0,0 +1,18 @@
+// Interactive Scene
+// Lavan
+// September 16, 2024
+
+// Creates and interactive scene
+
+stroke()
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(100, 180, 220);
+  fill(250,240,0);
+  circle(50, 50, 50);
+
+
+}
