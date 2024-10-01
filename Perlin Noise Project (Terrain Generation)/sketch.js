39c6764c6a247f6603cdd677b0fce7b5c024ce00
LavanSambavan <lavansambavan@gmail.com>
2024-10-01 12:16:29 -0600

    start

---
+++ Perlin Noise Project (Terrain Generation)/sketch.js
@@ -0,0 +1,13 @@
+// Perlin Noise Project (Terrain Generation)
+// Lavan 
+// October 1, 2024
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
