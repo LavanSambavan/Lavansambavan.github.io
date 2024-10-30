56bded5f4442f79f5de3e0dfb7c824ec292998c7
LavanSambavan <lavansambavan@gmail.com>
2024-10-30 12:16:51 -0600

    finished the last features
    Just need to fix how my cars and trucks looks

--- CARS CARS CARS/sketch.js
+++ CARS CARS CARS/sketch.js
@@ -7,29 +7,40 @@
 
 let eastbound = [];
 let westbound = [];
-
+let Light;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   for (let i = 0; i < 20; i++) {
-    eastbound.push(new Vehicle(random(windowWidth), random(500, 750), 1, 5));
-    westbound.push(new Vehicle(random(windowWidth), random(200, 450), -1, 5));
+    eastbound.push(new Vehicle(random(width), height / 2 + 20 + random(250), 1, 5));
+    westbound.push(new Vehicle(random(width), height / 2 - 40 + random(-250), -1, 5));
+  }
+  Light = new trafficLight();
+}
+
+function mouseClicked() {
+  if (keyIsDown(SHIFT)) {
+    // Left click for eastbound car
+    eastbound.push(new Vehicle(random(windowWidth), height / 2 + 20 + random(250), 1, 5));
+
+  }
+  else {
+    // Shift + left click for westbound car
+    westbound.push(new Vehicle(random(windowWidth), height / 2 - 30 + random(-250), -1, 5));
   }
 }
 
 function draw() {
   background(220);
   drawRoad();
+  Light.update();
+  Light.display();
   for (let i = 0; i < eastbound.length; i++) {
     eastbound[i].action();
   }
   for (let i = 0; i < westbound.length; i++) {
     westbound[i].action();
   }
-}
-
-function mouseClicked() {
-  eastbound.push(new Vehicle(random(windowWidth), random(600, 800), 1, 5));
 }
 
 function drawRoad() {
@@ -42,6 +53,7 @@ function drawRoad() {
     line(i, height / 2, i + 20, height / 2);
   }
 }
+
 
 class Vehicle {
   constructor(x, y, dir, xSpeed) {
@@ -57,7 +69,9 @@ class Vehicle {
   action() {
     // call the other functions like move, speed up etc..
     this.display();
-    this.move();
+    if (Light.state === "green") {
+      this.move();
+    }
     if (int(random(100)) === 1) {
       this.speedDown();
     }
@@ -69,25 +83,31 @@ class Vehicle {
     }
   }
 
-    display() {
-      if (this.type === 0) {
-        this.drawCar();
-      }
-      else if (this.type === 1) {
-        this.drawTruck();
-      }
+  display() {
+    if (this.type === 0) {
+      this.drawCar();
     }
-  
+    else if (this.type === 1) {
+      this.drawTruck();
+    }
+  }
+
   drawCar() {
     fill(this.c);
     noStroke();
-    rect(this.x, this.y, 30, 15);
+    rect(this.x, this.y, 40, 20); // car body
+    fill(60);
+    ellipse(this.x + 15, this.y + 20, 10, 10); // left wheel
+    ellipse(this.x + 25, this.y + 15, 10, 10); // right wheel
   }
 
   drawTruck() {
     fill(this.c);
     noStroke();
-    rect(this.x, this.y, 40, 20);
+    rect(this.x, this.y, 60, 30);
+    fill(60);
+    ellipse(this.x + 10, this.y + 30, 15, 15);
+    ellipse(this.x + 50, this.y + 30, 15, 15);
   }
 
   move() {
@@ -117,12 +137,42 @@ class Vehicle {
 
 }
 
-function mouseClicked() {
-  if (keyIsDown(SHIFT)) {
-    // Shift + left click for westbound car
-    westbound.push(new Vehicle(random(windowWidth), random(200, 450), -1, 5));
-  } else {
-    // Left click for eastbound car
-    eastbound.push(new Vehicle(random(windowWidth), random(600, 800), 1, 5));
+class trafficLight {
+  constructor() {
+    this.state = "green";
+    this.time = 0;
+  }
+
+  update() {
+    if (this.time > 0) {
+      this.time--;
+      if (this.time === 0) {
+        this.state = "green"; //resets the light to greem
+      }
+    }
+  }
+
+  display() {
+    if (this.state === "green") {
+      fill("green");
+    }
+    else {
+      fill("red");
+    }
+    noStroke();
+    rect(width / 2 - 25, height / 2 - 500, 100, 50);
+  }
+  toggle() {
+    if (this.state === "green") {
+      this.state = "red";
+      this.time = 120;
+    }
   }
 }
+
+function keyPressed() {
+  if (key === ' ') {
+    Light.toggle();
+  }
+}
+
