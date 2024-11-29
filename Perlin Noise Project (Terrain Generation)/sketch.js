b23ecff841f15dcfe40373d11b1d77cabc0e9ddc
LavanSambavan <lavansambavan@gmail.com>
2024-11-29 10:46:58 -0600

    start of perlin

--- Perlin Noise Project (Terrain Generation)/sketch.js
+++ Perlin Noise Project (Terrain Generation)/sketch.js
@@ -2,7 +2,8 @@
 // Lavan 
 // October 1, 2024
 
-let rectWidth = 200;
+let rectWidth = 1;
+let t=0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,19 +14,36 @@ function draw() {
   staircase()
 }
 
-function staircase(){
+function staircase() {
+  let windowSize = windowWidth * 2
+  let maxRect = windowWidth / windowSize
+  let maxHeight = 500;
+  let maxRectHeight = 0;
+  let maxX = 0;
+  let maxY = height
 
-  // let rectWidth = 4;
-  
-  for (let x = 0; x <= width; x += rectWidth) {
-    noFill();
-    
-    // Generate a height for each rectangle using noise
-    let noiseValue = noise(x * 0.01); // Scale the x value for more variety
-    let rectHeight = noiseValue * 1000; // Scale the height to fit the canvas
-    
-    // Draw the rectangle
-    rect(x, height, rectWidth, -rectHeight);
+  if (keyIsDown(LEFT_ARROW)) {
+    rectWidth -= 1
   }
+  if (keyIsDown(RIGHT_ARROW)) {
+    rectWidth += 1
+  }
+  if (rectWidth <= 1) {
+    rectWidth = 1;
+  }
+  else if(rectWidth >= width / 10){
+    rectWidth = width / 10;
+  }
+ for (let i = 0; i< windowSize; i++){
+  let x = i* maxRect;
+  let noiseValue = noise(i*0.01 + t);
+  let rectHeight = map(noiseValue, 0, 1, 0, maxHeight);
+  if(rectHeight>maxRectHeight){
+    maxRectHeight = rectHeight;
+    maxX = x;
+  }
+  fill(0);
+  rect(x, height-rectHeight,maxRect-1, rectHeight);
+ }
 }
 
