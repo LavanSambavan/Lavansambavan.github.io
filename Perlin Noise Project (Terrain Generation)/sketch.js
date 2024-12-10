9cff9263b216063e7919856a523bc7f5a0c63423
LavanSambavan <lavansambavan@gmail.com>
2024-12-10 12:18:47 -0600

    almost done perlin

--- Perlin Noise Project (Terrain Generation)/sketch.js
+++ Perlin Noise Project (Terrain Generation)/sketch.js
@@ -3,7 +3,8 @@
 // October 1, 2024
 
 let rectWidth = 1;
-let t=0;
+let t = 0;
+let maxHeight = 1000;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -15,12 +16,10 @@ function draw() {
 }
 
 function staircase() {
-  let windowSize = windowWidth * 2
-  let maxRect = windowWidth / windowSize
-  let maxHeight = 500;
   let maxRectHeight = 0;
   let maxX = 0;
   let maxY = height
+  let totalHeight = 0;
 
   if (keyIsDown(LEFT_ARROW)) {
     rectWidth -= 1
@@ -31,19 +30,34 @@ function staircase() {
   if (rectWidth <= 1) {
     rectWidth = 1;
   }
-  else if(rectWidth >= width / 10){
+  else if (rectWidth >= width / 10) {
     rectWidth = width / 10;
   }
- for (let i = 0; i< windowSize; i++){
-  let x = i* maxRect;
-  let noiseValue = noise(i*0.01 + t);
-  let rectHeight = map(noiseValue, 0, 1, 0, maxHeight);
-  if(rectHeight>maxRectHeight){
-    maxRectHeight = rectHeight;
-    maxX = x;
+  for (let x = 0; x < width; x += rectWidth) {
+    let noiseValue = noise(x * 0.01 + t);
+    let rectHeight = map(noiseValue, 0, 1, 0, maxHeight);
+
+    if (rectHeight > maxRectHeight) {
+      maxRectHeight = rectHeight;
+      maxX = x;
+      maxY = height - rectHeight;
+    }
+    fill(0);
+    rect(x, height - rectHeight, rectWidth - 1, rectHeight);
+    totalHeight += rectHeight;
   }
-  fill(0);
-  rect(x, height-rectHeight,maxRect-1, rectHeight);
- }
+
+  let averageHeigth = totalHeight / (width / rectWidth);
+  stroke(255, 0, 0);
+  line(0, height - averageHeigth, width, height - averageHeigth)
+  drawFlag(maxX + rectWidth / 2, maxY);
+
+  t += 0.01;
 }
 
+function drawFlag(x, y) {
+  stroke(0);
+  rect(x-2, y-50, 4, 50);
+  fill(255,0,0);
+  triangle(x, y - 50, x, y - 30, x + 20, y - 40);
+}
