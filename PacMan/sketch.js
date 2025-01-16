6cc1c86a79b23f302dcda38bc9fb0f6143dbeda1
LavanSambavan <lavansambavan@gmail.com>
2025-01-16 12:20:05 -0600

    got the pellets out of the middle started drawing pacman

--- PacMan/sketch.js
+++ PacMan/sketch.js
@@ -8,6 +8,8 @@
 let rows = 25;
 let cols = 25;
 let w, h;
+let pacX, pacY;  // pacmans position
+
 //Power ups place 5 randomly
 let powerUps = [
   { x: 1, y: 1 }, { x: 23, y: 1 }, { x: 4, y: 21 }, { x: 23, y: 23 }, { x: 20, y: 17 }, {x:1, y:6}
@@ -24,15 +26,15 @@ let maze = [
   "1100011111110101011011111", //9
   "1111010000000000011000011", //10
   "1110010101111111011011011", //11
-  "1110110101000001000011011",  //12
-  "0000110101000001011111000",//13
-  "1110000001000001000011011", //14
-  "1110111101111111011111011", //15
+  "1110110101222221000001011",  //12
+  "0000110101222221011101000", //13
+  "1110000001222221000001011", //14
+  "1110111101111111011101011", //15
   "1110000000000000000001011", //16
-  "1111110111101011101111001",  //17
+  "1111110111101011101111001", //17
   "1000110000001011101000101", //18
   "1010110111111000001011101", //19
-  "1010110111111111101000001",//20
+  "1010110111111111101000001", //20
   "1010110100000001101111111", //21
   "1010010101110100000000001", //22
   "1011110100000101111111101", //23
@@ -45,6 +47,10 @@ function setup() {
   rectMode(CORNER);
   w = width / cols;
   h = height / rows;
+
+  //pacmans starting position
+  pacX = 10;
+  pacY = 10;
 }
 
 function draw() {
@@ -52,7 +58,7 @@ function draw() {
   stroke(0);
   noFill();
   pellets();
-  powerUp();
+  eatGhosts();
   mazeDraw();
 }
 
@@ -62,6 +68,8 @@ function mazeDraw(){
       if (maze[i][j] === '1') {
         fill(0, 0, 255);  // Blue color for the walls
         rect(j * w, i * h, w, h);  // Draw the wall
+      } else if (maze[i][j] === '2') { // special grid
+        
       } else {
         noFill();  // Empty path, so no fill
         rect(j * w, i * h, w, h);  // Draw the empty cell
@@ -82,10 +90,15 @@ function pellets() {
   }
 }
 
-function powerUp() {
+function eatGhosts() {
   fill(255);
   for (let i = 0; i < powerUps.length; i++) {
-    let powerup = powerUps[i];
-    ellipse(powerup.x * w + w / 2, powerup.y * h + h / 2, w / 2, h / 2);
+    let power = powerUps[i];
+    ellipse(power.x * w + w / 2, power.y * h + h / 2, w / 2, h / 2);
   }
+}
+
+function drawPacMan(){
+  ellipse(pacX, pacY, 10, 10)
+  fill(0,0,100);
 }
